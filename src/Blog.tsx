80ffde9b5626edd8a6de10f8aa2913571e10ce62
parcel <genericpb@gmail.com>
2026-01-11 14:01:31 -0600

    godot code

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -75,8 +75,58 @@ the top part is a basic unshaded material with alpha
 and then the bottom part is the decals  
 <br />
 
-the shader i use in Godot is still a WIP.  
-thankfully Godot supports having 2 UVs on a mesh, so all you have to do in the shader is just read from them, then composite them together. takes a little work but isn't too hard. maybe i can share it soon.  
+the shader i use in Godot is here:  
+\`\`\`
+shader_type spatial;
+render_mode unshaded, fog_disabled, cull_disabled;
+
+uniform sampler2D tex: source_color;
+uniform bool swap_uv_maps;
+
+// A D
+// C B
+float dither(int x, int y, float density) {
+    bool a = density > 0.875;
+    bool b = density > 0.625;
+    bool c = density > 0.375;
+    bool d = density > 0.125;
+
+    int xm = x % 2;
+    int ym = y % 2;
+
+    bool is_on = a && xm == 0 && ym == 0 ||
+                 b && xm == 1 && ym == 1 ||
+                 c && xm == 1 && ym == 0 ||
+                 d && xm == 0 && ym == 1;
+
+    if (is_on) {
+        return 1.0;
+    } else {
+        return 0.0;
+    }
+}
+
+void fragment() {
+    vec2 uv_front = swap_uv_maps ? UV : UV2;
+    vec2 uv_back = swap_uv_maps ? UV2 : UV;
+    vec4 base_layer = texture(tex, uv_front);
+    vec4 decal_layer = texture(tex, uv_back);
+    vec4 mixed = mix(base_layer, decal_layer, decal_layer.a);
+
+    ALBEDO = mixed.rgb;
+
+    float a = clamp(base_layer.a + decal_layer.a, 0.0, 1.0);
+
+    // Undithered alpha
+    // ALPHA = a;
+
+    // Dithered alpha
+    ALPHA = dither(int(FRAGCOORD.x), int(FRAGCOORD.y), a);
+
+    ALPHA_SCISSOR_THRESHOLD = 0.5;
+}
+\`\`\`
+thankfully Godot supports having 2 UVs on a mesh, so all you have to do in the shader is just read from the texture with them, then composite the values together. takes a little work but isn't too hard.  
 <br />
 
 i got it working in Unity too. something along the lines of:  
