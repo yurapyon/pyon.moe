b95425350e33daaa8c2202b50cca3e0c5eb35f1c
parcel <genericpb@gmail.com>
2026-03-16 00:35:42 -0500

    Loppy

--- frontend/src/Loppy.tsx
+++ frontend/src/Loppy.tsx
@@ -3,11 +3,19 @@ import { SubNav } from "./components/SubNav";
 import { Panel } from "./components/Panel";
 
 const topBody = `
-a 3d editor for the masses
+a 3d editor for the masses  
+<a href="https://codeberg.org/yurapyon/loppy" target="_blank">codeberg</a>
+`;
+
+const goalsBody = `
+- easy to learn and quick to use
+- integrate better with gamedev project management
+- preview glsl shaders and scripts live
+- easy to manage materials, textures, etc
 `;
 
 const FAQBody = `
-*Gen AI?*  
+*GenAI plugins?*  
 not a chance  
 <br />
 *When will it be released?*  
@@ -19,6 +27,7 @@ export const Loppy: Component = () => {
     <div class="flex flex-col gap-4">
       <SubNav />
       <Panel title="Loppy" titleClass="bg-lmn-pink" body={topBody} />
+      <Panel title="goals" titleClass="bg-lmn-blue" body={goalsBody} />
       <Panel title="FAQ" titleClass="bg-lmn-green" body={FAQBody} />
     </div>
   );
